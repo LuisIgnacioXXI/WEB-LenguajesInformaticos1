76b44a97aadbb714ddec0256de94d31defbd2b96
Gambito <facundomariano.gamboa@estudiantes.unahur.edu.ar>
2026-06-27 15:05:13 -0300

    commit 4: Javascript, modo oscuro y validacion de formulario

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const boton = document.getElementById("btnModoOscuro");
 
+
+
 if (localStorage.getItem("modo") === "oscuro") {
     document.body.classList.add("modo-oscuro");
     boton.textContent = "☀️ Modo claro";
@@ -19,4 +21,21 @@ boton.addEventListener("click", function () {
 
 })
 
+const formulario = document.getElementById("formContacto");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nombre = document.getElementById("formNombre").value.trim();
+    const email = document.getElementById("formEmail").value.trim();
+    const programa = document.getElementById("formPrograma").value;
+    const mensaje = document.getElementById("formMensaje").value.trim();
+
+    if(nombre === "" || email === "" || programa === "" || mensaje === ""){
+        event.preventDefault();
+        alert("Debe completar todos los campos.");
+    }
+
+    
+});
 
